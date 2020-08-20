21f91d0673de2e62dec6e871b4b9c0a8011788db
Bharath Raj Kumar <bharath@incend.in>
2020-08-20 11:22:27 +0530

    first commit

---
+++ Wikisearch/SearchModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchModel.swift
+//  Wikisearch
+//
+//  Created by Bharath  Raj kumar on 18/08/20.
+//  Copyright © 2020 Learn. All rights reserved.
+//
+
+import Foundation
---
+++ Wikisearch/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  Wikisearch
+//
+//  Created by Bharath  Raj kumar on 17/08/20.
+//  Copyright © 2020 Learn. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
